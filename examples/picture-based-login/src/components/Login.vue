1476ff96bbc33a863afe9966267f1325de8649a4
Sam Billingham <sambillie.sb@gmail.com>
2020-05-20 21:32:19 +0200

    add: (picture-based-login) - initial picture login example

---
+++ examples/picture-based-login/src/components/Login.vue
@@ -0,0 +1,131 @@
+<template>
+ <div>
+   	<div class="picture-login">
+		<h1>Student Sign in</h1>
+     	<!-- <input required v-model="id" type="text" placeholder="ID Number"/> -->
+		<div class="picture-login__password">
+			<template v-for="(number, index) in pass">
+				<img :src="require(`@/assets/${number}.png`)"  :key="index">
+			</template>
+			<template v-for="(number, index) in placeholder">
+				<div class="picture-login__placeholder" :key="index"></div>
+			</template>
+		</div>
+		<div class="picture-login__numbers">
+			<img v-for="(img, index) in images" :key="img" :src="img" @click="imgSelected(index)">
+		</div>
+		<a class="btn" @click="login">Login</a>
+	 </div>
+     
+ </div>
+</template>
+
+<script>
+	export default {
+		data(){
+			return {
+				id : '',
+				placeholder: [0,0,0,0,0],
+				pass: [],
+				letters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','u','r','s','t','v','w','x','y','z'],
+				images : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','u','r','s','t','v','w','x','y','z'].map( x => require(`@/assets/${x}.png`) ),
+            }
+		},
+		watch: {
+			pass(val) {
+				this.id = val.join('');
+			},
+		},
+		methods: {
+			imgSelected(index){
+				if(this.pass.length <5){
+					this.pass.push( this.letters[index]);
+					this.placeholder.pop()
+				}
+			},
+		 	login() {
+		   		this.$store.dispatch('login', { id: this.id })
+		   		.then(() => this.$router.push('/'))
+		   		.catch(err => console.log(err))
+		   	}
+		}
+	}
+</script>
+
+<style scoped>
+
+form {
+	max-width: 400px;
+	width: 100%;
+	margin: 0 auto;
+	padding: 3rem;
+}
+label, input {
+	display: block;
+	text-align: left;
+	width: 100%;
+	margin: 0.5rem;
+}
+
+.picture-login {
+	padding: 2rem;
+	background: #95E1E7;
+	max-width: 50rem;
+	margin: 0 auto;
+	border-radius: 0.5rem;
+}
+
+.picture-login h1 {
+	margin-bottom: 2rem;
+}
+
+.picture-login__password {
+	margin-bottom: 2rem;;
+}
+
+.picture-login__password img,
+.picture-login__placeholder {
+	height: 4rem;
+	width: 4rem;
+	border-radius: 8px;
+	margin: 0.5rem;
+	cursor: pointer;
+	border: transparent 3px solid;
+	display: inline-block;
+}
+.picture-login__placeholder {
+	border: #444 3px solid;
+}
+
+.picture-login__numbers {
+    flex-direction: column;
+    display: flex;
+    max-height: 300px;
+    flex-wrap: wrap;
+	overflow: scroll;
+}
+
+.picture-login__numbers img {
+	height: 8rem;
+	width: 8rem;
+	border-radius: 8px;
+	margin: 0.5rem;
+	cursor: pointer;
+	border: transparent 3px solid;
+}
+
+.picture-login_numbers img:hover {
+	border: dotted 3px #444;
+}
+.btn {
+	background: #4CAF50;
+	color: #fff;
+	border-radius: 8px;
+	padding: 0.5rem 1rem;
+	margin-top: 2rem;
+	display: inline-block;
+	font-weight: 700;
+	cursor: pointer;
+}
+
+</style>
